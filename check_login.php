82e083c64861c09774d18b9622abbb8ea147d1ac
VincentHo1689 <abc1234hkc@gmail.com>
2024-04-08 15:49:22 +0800

    testing login

--- check_login.php
+++ check_login.php
@@ -9,12 +9,25 @@
     $name = $_POST['username'];
     $pw = $_POST['pw'];
     
+    echo "Test Input: ";
+    echo "<br> Role: ".$role;
+    echo "<br> Name: ".$name;
+    echo "<br> PW: ".$pw;
+    echo "<br><br> Result: ";
+    
+
     if (empty($role)){
         # no input radio 
         echo "no input!";}
     else if($role='s'){ 
         $sql = "SELECT PW FROM Students WHERE username = ".$name;
         $result = mysqli_query($conn, $sql);
+
+        if (!$conn)
+        {
+            die("Connect Error:" . mysqli_connect_error());
+        }
+
         if ($result->num_rows > 0) {
             # username wrong/ do not exist
             echo "x Username";
