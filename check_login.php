455d31ea645031acf4a8c230ff5e456ae4e3d82a
VincentHo1689 <abc1234hkc@gmail.com>
2024-04-08 15:18:20 +0800

    Change login 2

---
+++ check_login.php
@@ -0,0 +1,13 @@
+<?php
+    $conn = mysqli_connect("localhost", "root","","COMP3421");
+    if (!$conn)
+    {
+        die("Connect Error:" . mysqli_connect_error());
+    }
+
+    $role = $_POST['login_role'];
+    $name = $_POST['username'];
+    $pw = $_POST['pw'];
+  
+    echo $role."<br>".$name."<br>".$pw
+  ?>
