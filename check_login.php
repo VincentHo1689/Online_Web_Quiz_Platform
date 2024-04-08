7bf44c3913ace6e062979b0f253a77f911e342a0
VincentHo1689 <abc1234hkc@gmail.com>
2024-04-08 15:40:13 +0800

    making the login logic gate (students)

--- check_login.php
+++ check_login.php
@@ -10,7 +10,30 @@
     $pw = $_POST['pw'];
     
     if (empty($role)){
+        # no input radio 
         echo "no input!";}
+    else if($role='s'){ 
+        $sql = "SELECT PW FROM Students WHERE username = ".$name;
+        $result = mysqli_query($conn, $sql);
+        if ($result->num_rows > 0) {
+            # username wrong/ do not exist
+            echo "x Username";
+        }
+        else {
+            # username exists
+            $row = $result->fetch_assoc();
+            echo "row = ".$row;
+            if ($pw = $row){
+                #correct password
+                echo "v Username v Password";
+            }
+            else {
+                #wrong password
+                echo "v Username x Password";
+            }
+        }
+    }
+    else {}
 
     echo empty($role)."<br>";
     echo $role."<br>".$name."<br>".$pw
