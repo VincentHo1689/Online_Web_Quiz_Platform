fa4e1b4a28ed2ccedb8786a114b889d434ae6fbc
VincentHo1689 <abc1234hkc@gmail.com>
2024-04-08 17:54:09 +0800

    Signup PHP done

--- signup.php
+++ signup.php
@@ -11,7 +11,12 @@
     $email = $_POST['email'];
     $pw = $_POST['pw'];
     $cpw = $_POST['cpw'];
-    echo $role;
+
+    $sql = "SELECT MAX(StudentID) AS new_id FROM Students";
+    $result = mysqli_query($conn, $sql);
+    
+    $row = $result->fetch_assoc();
+    $new_id = (int) $row["new_id"] +1;
 
     if (empty($role)){
         # no input radio 
@@ -23,10 +28,16 @@
         echo "<script> alert('Password not identical!') 
         document.location='signup.html'</script>";}
 
-    if ($role=='Students'){
+    if (empty($name) || empty($email) || empty($pw)){
+        echo "<script> alert('Some field are not entered!') 
+        document.location='signup.html'</script>";
+    }
+    else{
         echo "<script> 
-        alert('Student account successfully created! \\n Username = $name \\n Email = $email \\n Password = $pw ')
+        alert('$role account successfully created! \\n Username = $name \\n Email = $email \\n Password = $pw ')
         </script>";
-        #document.location='login.html'";
+        $sql = "INSERT INTO $role VALUES ('$new_id','$name','$pw','$email')";
+        mysqli_query($conn, $sql);
+        echo "<script> document.location='login.html' </script>";
     }
   ?>
