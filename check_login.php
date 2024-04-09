beb28ca7c4a6853bf065dd124aeeb7196561ee34
VincentHo1689 <abc1234hkc@gmail.com>
2024-04-09 15:24:39 +0800

    Added debug message

--- check_login.php
+++ check_login.php
@@ -5,7 +5,9 @@
         die("Connect Error:" . mysqli_connect_error());
     }
 
-    
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
 
     $role = $_POST['login_role'];
     $name = $_POST['username'];
