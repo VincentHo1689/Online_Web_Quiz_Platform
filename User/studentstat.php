a9d84c8908d5f939356e3035bb1aec8495e3badc
VincentHo1689 <abc1234hkc@gmail.com>
2024-04-12 17:08:18 +0800

    finished studentstat

--- User/studentstat.php
+++ User/studentstat.php
@@ -8,7 +8,7 @@
   <title>Main Page</title>
   <link rel="icon" href="">
 </head>
-<body onload="document.getElementById('name').innerHTML='Welcome '+getCookie('username');" style="background-color: bisque;">
+<body style="background-color: bisque;">
   <div class="topnav">
     <a href="../index.html">AlphaQuiz</a>
     <a href="../login/login.html" class="split">Logout</a>
@@ -33,18 +33,18 @@
         $SID = $_COOKIE["ID"];
 
         $conn = mysqli_connect("localhost", "root","","COMP3421");
-        $sql = "SELECT DISTINCT Quiz.name, FROM Quiz,Question,Stat 
-        WHERE Stat.StudentID ='$SID' AND Stat.QuestionID = Question.QuestionID AND Question.QuizID = Quiz.QuizID"; 
+        $sql = "SELECT DISTINCT Q.name FROM Quiz AS Q,Question AS QU ,Stat AS S 
+        WHERE S.StudentID ='$SID' AND S.QuestionID = QU.QuestionID AND QU.QuizID = Q.QuizID"; 
         $result = mysqli_query($conn, $sql);
         while($row = mysqli_fetch_assoc($result))
         {
-        $quizName=$row['name'];
-        $sql2 = "SELECT COUNT(*) FROM Quiz,Question,Stat
-        WHERE Stat.StudentID ='$SID' AND Stat.QuestionID = Question.QuestionID AND Question.QuizID = Quiz.QuizID
-        AND Quiz.QuizID ='$quizName' AND Stat.Correct = 1";
-        $sql3 = "SELECT COUNT(*) FROM Quiz,Question,Stat
-        WHERE Stat.StudentID ='$SID' AND Stat.QuestionID = Question.QuestionID AND Question.QuizID = Quiz.QuizID
-        AND Quiz.QuizID ='$quizName'";
+        $quizName=$row['Q.name'];
+        $sql2 = "SELECT COUNT(*) FROM Quiz AS Q,Question AS QU ,Stat AS S 
+        WHERE S.StudentID ='$SID' AND S.QuestionID = QU.QuestionID AND QU.QuizID = Q.QuizID
+        AND Q.name ='$quizName' AND S.Correct = 1";
+        $sql3 = "SELECT COUNT(*) FROM Quiz AS Q,Question AS QU ,Stat AS S
+        WHERE S.StudentID ='$SID' AND S.QuestionID = QU.QuestionID AND QU.QuizID = Q.QuizID
+        AND Q.name ='$quizName'";
         echo "<tr>
                 <td>$quizName</td>
                 <td>mysqli_query($conn, $sql2)</td>
