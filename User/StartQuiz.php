47c1f47bcb742f3fa92ada91ae44ccb363ea8442
Alpha-No <jochunyat@gmail.com>
2024-04-12 17:11:57 +0800

    startquiz done

--- User/StartQuiz.php
+++ User/StartQuiz.php
@@ -9,26 +9,34 @@
   <link rel="icon" href="">
 </head>
 <body onload="function()">
-<?php 
-  
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
-  
-    $name = $_POST["QuizName"];
-    echo $name;
+  <?php 
     
-?>
-  <table>
-    <Tr>
-      <th>Quiz Name</th>
-      <th>Class Name</th>
-    </tr>
+      ini_set('display_errors', '1');
+      ini_set('display_startup_errors', '1');
+      error_reporting(E_ALL);
     
-    
- 
-  </table>
+      $QuizName = $_POST["QuizName"];
+      
+      $conn = mysqli_connect("localhost", "root","","COMP3421");
+      $sql = "SELECT QuizID FROM Quiz WHERE name = '$QuizName'";
+      $result = mysqli_query($conn, $sql);
+      $row = $result->fetch_assoc();
+      $QuizID = $row['QuizID'];
+      echo "<script>  document.cookie = 'QuizID = $QuizID; path=/'; </script>";
 
-  <a id="journey" class="tbhover tbcontent" href="main_s.html">Back to main page</a>!
+      $sql = "SELECT COUNT(*) AS cnt FROM Question WHERE QuizID = '$QuizID'";
+      $result = mysqli_query($conn, $sql);
+      $row = $result->fetch_assoc();
+      $QuizNum = $row['cnt'];
+  ?>
+
+
+  <h1><?= "There are total ".$QuizNum." questions."?></h1>
+
+  <form action="StartQuiz.php" method="post">
+    <td><input type="submit" value="Start Quiz"></td>
+    <input type="hidden" name='QuizID' value='<?= $QuizID ?>'>
+  </form>
+
 </body>
 </html>
