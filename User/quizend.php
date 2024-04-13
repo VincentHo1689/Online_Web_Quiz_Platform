ba439e13caf3a9799ebb312b3b3884e4807a789a
VincentHo1689 <abc1234hkc@gmail.com>
2024-04-13 16:01:15 +0800

    added back to main page

--- User/quizend.php
+++ User/quizend.php
@@ -9,43 +9,46 @@
   <link rel="icon" href="">
 </head>
 <body>
-<?php
-    $conn = mysqli_connect("localhost", "root","","COMP3421");
-    if (!$conn)
-    {
-        die("Connect Error:" . mysqli_connect_error());
-    }
+    <?php
+        $conn = mysqli_connect("localhost", "root","","COMP3421");
+        if (!$conn)
+        {
+            die("Connect Error:" . mysqli_connect_error());
+        }
 
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
+        ini_set('display_errors', '1');
+        ini_set('display_startup_errors', '1');
+        error_reporting(E_ALL);
 
-    $QuizID = $_COOKIE['QuizID'];
-    $StudentID = $_COOKIE['ID'];
+        $QuizID = $_COOKIE['QuizID'];
+        $StudentID = $_COOKIE['ID'];
 
-    $QNum = $_COOKIE['QNum'];
-    $sql = "SELECT Content, QuestionID FROM Question WHERE QuizID = '$QuizID' ORDER BY QuestionID";
-    $Question = mysqli_query($conn, $sql);
-    for ($i = 0; $i < $QNum; $i++) {
-        $row = $Question->fetch_assoc();
-    }
-    $QuestionID = $row['QuestionID'];
+        $QNum = $_COOKIE['QNum'];
+        $sql = "SELECT Content, QuestionID FROM Question WHERE QuizID = '$QuizID' ORDER BY QuestionID";
+        $Question = mysqli_query($conn, $sql);
+        for ($i = 0; $i < $QNum; $i++) {
+            $row = $Question->fetch_assoc();
+        }
+        $QuestionID = $row['QuestionID'];
 
-    $SID = $_COOKIE['ID'];
-    
-    # Check last question
-    
-    $sql = "SELECT COUNT(Correct) AS Total, SUM(Correct) AS Correct FROM Stat WHERE QuestionID IN( SELECT QuestionID FROM Question WHERE QuizID = '$QuizID') AND StudentID ='$StudentID';";
-    $result = mysqli_query($conn, $sql);
-    $row = $result->fetch_assoc();
-    $total = $row['Total'];
-    $correct = $row['Correct'];
-?>
-    <h1>You score <?= $correct?> out of <?= $total?> ! </h1>
-<?php
-    setcookie('QNum','', time()-3600, '/');
-    setcookie('QuizID','', time()-3600, '/');
-?>
+        $SID = $_COOKIE['ID'];
+        
+        # Check last question
+        
+        $sql = "SELECT COUNT(Correct) AS Total, SUM(Correct) AS Correct FROM Stat WHERE QuestionID IN( SELECT QuestionID FROM Question WHERE QuizID = '$QuizID') AND StudentID ='$StudentID';";
+        $result = mysqli_query($conn, $sql);
+        $row = $result->fetch_assoc();
+        $total = $row['Total'];
+        $correct = $row['Correct'];
+    ?>
+        <h1>You score <?= $correct?> out of <?= $total?> ! </h1>
+    <?php
+        setcookie('QNum','', time()-3600, '/');
+        setcookie('QuizID','', time()-3600, '/');
+    ?>
+    <form action="main_s.html" id="quiz" method="post">
+        <input type='submit' value='Back to Main Page'>
+    </form>
 </body>
 
 
