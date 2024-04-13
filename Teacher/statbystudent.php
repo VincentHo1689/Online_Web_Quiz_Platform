57982fd69b8cf7e363f198bd365749d3292f0f59
VincentHo1689 <abc1234hkc@gmail.com>
2024-04-13 21:42:11 +0800

    userstat

--- Teacher/statbystudent.php
+++ Teacher/statbystudent.php
@@ -15,10 +15,10 @@
     <a href="../other/aboutus.html" class="split">About Us</a>
     <a href="main_s.html" class="split">Home</a>
   </div>
-<div style="margin: auto; width: 100%; text-align:center;">
-<div><h1 style="display: inline-block;">Past Statistics</h1></div>
-<div class="userstat" style="margin: auto; text-align:center;">
-  <table class="tabletext">
+
+<h1>Past Statistics</h1>
+<div class="userstat">
+<table class="tabletext">
         <tr>
           <th>Quiz Name</th>
           <th>Score</th>
@@ -30,7 +30,7 @@
         ini_set('display_startup_errors', '1');
         error_reporting(E_ALL);
       
-        $SID = $_COOKIE["ID"];
+        $TeacherID = $_COOKIE["ID"];
 
         $conn = mysqli_connect("localhost", "root","","COMP3421");
         $sql = "SELECT DISTINCT Q.name AS name FROM Quiz AS Q,Question AS QU ,Stat AS S 
@@ -45,20 +45,21 @@
         $result2 = mysqli_query($conn, $sql2);
         $result2 = mysqli_num_rows($result2);
         ?> <tr>
-                <td><?php echo $quizName ?></td>
-                <td><?php echo $result2 ?></td> 
-        <?php 
-        $sql3 = "SELECT * FROM Quiz AS Q, Question AS QU ,Stat AS S 
-        WHERE S.StudentID ='$SID' AND S.QuestionID = QU.QuestionID AND QU.QuizID = Q.QuizID
-        AND Q.name ='$quizName'";
-        $result3 = mysqli_query($conn, $sql3);
-        $result3 = mysqli_num_rows($result3); ?>
-                <td><?php echo $result3 ?></td>
-        </tr>
-        <?php }
-        ?>  
+            <td><?php echo $quizName ?></td>
+            <td><?php echo $result2 ?></td> 
+
+            <?php 
+            $sql3 = "SELECT * FROM Quiz AS Q, Question AS QU ,Stat AS S 
+            WHERE S.StudentID ='$SID' AND S.QuestionID = QU.QuestionID AND QU.QuizID = Q.QuizID
+            AND Q.name ='$quizName'";
+            
+            $result3 = mysqli_query($conn, $sql3);
+            $result3 = mysqli_num_rows($result3); ?>
+
+            <td><?php echo $result3 ?></td>
+          </tr>
+        <?php }?>  
       </table>
-        </div>
         </div>
 </body>
 </html>
